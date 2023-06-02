f8cee6b016c27f4c6592d01e066be8e7c4a19e60
harishsinghbreathingroomfoundation <harish_singh+breathingroomfoundation@technologymindz.com>
2023-06-02 15:43:48 +0530

    committed

---
+++ src/main/java/abcorg/finit/App.java
@@ -0,0 +1,13 @@
+package abcorg.finit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
